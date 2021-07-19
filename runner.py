9abb42e6b20c618389ed20d11f925068d715da34
Simon Renblad <simon.renblad@outlook.com>
2021-07-19 15:36:55 +0200

    attempting to create executable

--- runner.py
+++ runner.py
@@ -1,6 +1,18 @@
 from os import kill
-import sys, pygame, sample
+import sys, pygame
 from pygame.locals import *
+import random
+
+def sample():
+    sample_words = 250
+    file_path_name = "resources/words-alpha.txt"
+
+    with open(file_path_name) as f:
+        word_list = f.read().splitlines()
+
+    sample_list = random.sample(word_list, sample_words)
+
+    return sample_list
 
 LINE_LENGTH = 40
 NUM_LINES = 3
@@ -54,54 +66,56 @@ class Paragraph():
     def reachedEnd(self, index):
         return (index+1) not in range(len(self.letters))
 
+def run():
+    pygame.init()
 
-pygame.init()
+    size = width, height = 1200, 800
 
-size = width, height = 1200, 800
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Runner')
+    font = pygame.font.Font('font/AnonymousPro-Regular.ttf', FONT_SIZE)
+    set_sample = sample()
+    paragraph = Paragraph(set_sample, -1)
+    last_end = paragraph.end_word
 
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Runner')
-font = pygame.font.Font('font/AnonymousPro-Regular.ttf', FONT_SIZE)
-set_sample = sample.sample()
-paragraph = Paragraph(set_sample, -1)
-last_end = paragraph.end_word
+    cursor_location = 0
 
-cursor_location = 0
+    running = True
+    while(running):
 
-running = True
-while(running):
+        screen.fill(pygame.Color("white"))
+        
+        for l in paragraph.letters:
+            color_c = (0,0,0)
+            let = l.letter
+            if l.cursor_on:
+                color_c = pygame.Color("red")
+                if let == " ":
+                    let = "_"
+            elif l.cursor_passed:
+                color_c = pygame.Color("gray")
+            else:
+                color_c = pygame.Color("black")
+            text = font.render(let, True, color_c)
+            text_rect = text.get_rect(center=(l.x*FONT_SIZE_WIDTH+OFFSET, l.y*FONT_SIZE_HEIGHT+OFFSET))
+            screen.blit(text, text_rect)
 
-    screen.fill(pygame.Color("white"))
-    
-    for l in paragraph.letters:
-        color_c = (0,0,0)
-        let = l.letter
-        if l.cursor_on:
-            color_c = pygame.Color("red")
-            if let == " ":
-                let = "_"
-        elif l.cursor_passed:
-            color_c = pygame.Color("gray")
-        else:
-            color_c = pygame.Color("black")
-        text = font.render(let, True, color_c)
-        text_rect = text.get_rect(center=(l.x*FONT_SIZE_WIDTH+OFFSET, l.y*FONT_SIZE_HEIGHT+OFFSET))
-        screen.blit(text, text_rect)
-
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if chr(event.key) == paragraph.getLetter(cursor_location):
-                #move cursor
-                if paragraph.reachedEnd(cursor_location):
-                    # create new paragraph
-                    paragraph = Paragraph(set_sample, last_end)
-                    last_end = paragraph.end_word
-                    cursor_location = 0
-                else:
-                    cursor_location += 1
-                    paragraph.moveCursor(cursor_location)
-    
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if chr(event.key) == paragraph.getLetter(cursor_location):
+                    #move cursor
+                    if paragraph.reachedEnd(cursor_location):
+                        # create new paragraph
+                        paragraph = Paragraph(set_sample, last_end)
+                        last_end = paragraph.end_word
+                        cursor_location = 0
+                    else:
+                        cursor_location += 1
+                        paragraph.moveCursor(cursor_location)
+        
+if __name__ == "__main__":
+    run()
